25e885853758e22a9d5168c93480358a4641d727
Patrik121212 <patrik.schlegel@gritacademy.se>
2026-04-01 21:44:56 +0200

    Uppgift 1

---
+++ Tillfälle 1 2026-03-30/scripts/script.js
@@ -0,0 +1,173 @@
+let foo = 0
+let bar = 0
+
+console.log(foo/bar)
+
+//----//
+
+//Här nedan är bara massa anteckningar och test//
+
+/*
+//alert(123)
+//document.writeln(456)
+//document.writeln("hejsan")
+console.log(789)
+console.log(10000000000)
+//console.log("hejsan")
+//console.log("hallå där")
+
+//skapa en behållare som i detta fall heter box, tilldela denna box nummer 5
+//let box = 5
+//box = 10 ändrar vi till att den inehåller 10 istället
+/*box = "hello world"
+
+console.log(box)
+
+box = "alrik är cool"
+
+console.log(box)
+
+//let foo = 88// deklarera en variabel som heter foo, (let el. var)
+//foo = 88 // tilldelar 88 till variabeln foo
+
+console.log(1+1)
+console.log(2*2)
+console.log(foo+22)*/
+
+/*1:58 in i lektion 1 (termenr vi ska känna till)
+
+VIKTIGT ATT KUNNA FÖLJANDE TERMER
+
+Deklarera = skapa variabler. Ex let foo
+Tilldela = ger variabeln ett värde/namn. Ex foo = 88
+
+Ex på fråga: 
+Deklarera en variabel som heter poäng. (Det betyder skapa en variabel.)
+Tilldela poäng 100, alltså ge variabeln 100 som värde.
+Såhär: let poäng = 100
+ 
+*/
+
+/*let poäng = 100
+console.log(poäng+foo)
+
+box = 15
+console.log(box+foo)
+
+let namn = "james"
+console.log(namn)*/
+
+//let foo = 0
+//let bar = 0
+
+//let result = "You cant divide us"
+
+//let result = foo/bar
+//console.log(result)
+
+//*console.log(foo-bar)
+
+/*{let foo = 10
+
+console.log(foo-bar)
+}
+
+console.log(foo-bar)
+
+foo = 1+1
+
+console.log(foo)
+
+console.log(foo)
+
+let Låda
+låda = "hej"+"san"
+låda = 2+2*3
+
+console.log(låda)
+
+låda = "hej"+"san"
+
+console.log(låda)
+
+let box1
+
+box1 = 2+2*3
+
+console.log(box1)
+
+// P arantes
+// E xponent
+// M ultiplikation
+// D ivition
+// A ddition
+// S ubstraktion
+
+box1 = (2+2)*3
+
+console.log(box1)
+
+box1 = "hej" + 1
+
+console.log(box1)
+
+box1 = "hej"+1+1
+
+console.log(box1)
+
+box1 = "hej"+(1+1)
+
+console.log(box1)
+
+box1 = 1+1+"hej"
+
+console.log(box1)
+
+box2 = 2
+
+console.log(box2)
+
+let box3 = 2
+
+console.log(box3)
+
+box4 = 2
+
+console.log(box4)
+
+//let x = 10
+//let y = 2
+//let z = x / y
+
+//console.log(z)
+
+//let text1 ="patrik "
+//let text2 ="schlegel"
+//let text3 = text1 + text2
+
+//console.log(text3)
+
+let text4 = "what a nice "
+text4 += "nice day"
+
+console.log(text4)
+
+///let x = 5
+//let result = x > 8
+
+//console.log(result)
+
+let text1 = "A"
+let text2 = "B"
+
+//let result = text1 < text2
+
+console.log(result)
+
+let x = 5
+let y = "8"
+let z = x + y
+
+console.log(z)
+
+//alert(divide) */
